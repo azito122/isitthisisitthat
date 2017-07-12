5253da7ab04ff5a9caad7dbef76a7dc4311cb36d
Andrew Zito <azito122@gmail.com>
2017-07-12 00:10:25 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+
+<?php include "head.php" ?>
+
+<body>
+
+	<div class="main container">
+
+		<div class="token container">
+			
+			<div class="img"></div>
+			<h1>Chimpanzee</h1>
+
+		</div>
+
+		<div class="tags container">
+
+			<label>
+				<span>Test 1</span>
+				<input type="checkbox" name="tags" value="">
+				</label>
+			<label>
+				<span>Test 2</span>
+				<input type="checkbox" name="tags" value="">
+				</label>
+			<label>
+				<span>Test 3</span>
+				<input type="checkbox" name="tags" value="">
+				</label>
+
+		</div>
+
+	</div>
+
+</body>
